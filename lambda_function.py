43fb7c4aed29a2f2f7a32545567e85665d1206e2
yaron-cider <100203813+yaron-cider@users.noreply.github.com>
2024-11-28 14:57:37 +0200

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,17 +1,19 @@
 import json
 from datetime import datetime
-
+import requests
 def lambda_handler(event, context):
     # Get current timestamp
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     
     # Create greeting message
     message = f"Hello! שלום Salam Current timestamp is: {current_time}"
+
+    response = requests.get("https://whatthefuckismyip")
     
     # Return response
     return {
         'statusCode': 200,
         'body': json.dumps({
-            'message': message
+            'message': message + " " + response.content
         })
     }
