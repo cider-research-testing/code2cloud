6f4d1bc4e044662be0bf8009954d25119197f84f
yaron-cider <100203813+yaron-cider@users.noreply.github.com>
2024-12-09 15:51:24 +0200

    Update deploy-lambda.tf

--- deploy-lambda.tf
+++ deploy-lambda.tf
@@ -24,7 +24,7 @@ resource "aws_lambda_function" "test_lambda" {
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.lambda_handler"
  
-  source_code_hash = data.archive_file.lambda.output_base64sha256
+  source_code_hash = filebase64sha256("function.zip")
  
   runtime = "python3.13"  
 }
