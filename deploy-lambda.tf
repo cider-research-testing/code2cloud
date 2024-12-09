8b249c98dca5ea15adab3baa7ee4c89a8beb71b1
yaron-cider <100203813+yaron-cider@users.noreply.github.com>
2024-12-09 15:42:43 +0200

    Update deploy-lambda.tf

--- deploy-lambda.tf
+++ deploy-lambda.tf
@@ -25,18 +25,12 @@ data "archive_file" "lambda" {
 resource "aws_lambda_function" "test_lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
-  filename      = "lambda_function_payload.zip"
-  function_name = "lambda_function_name"
+  filename      = "function.zip"
+  function_name = "greeting-timestamp-lambda_3_tf"
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "index.test"
+  handler       = "lambda_function.lambda_handler"
  
   source_code_hash = data.archive_file.lambda.output_base64sha256
  
-  runtime = "nodejs18.x"
- 
-  environment {
-    variables = {
-      foo = "bar"
-    }
-  }
+  runtime = python3.13"  
 }
