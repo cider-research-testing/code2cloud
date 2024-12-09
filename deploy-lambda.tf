c97c0461ce30cbda66e17ad9c82e2a34c9506394
yaron-cider <100203813+yaron-cider@users.noreply.github.com>
2024-12-09 15:54:39 +0200

    Update deploy-lambda.tf

--- deploy-lambda.tf
+++ deploy-lambda.tf
@@ -9,11 +9,6 @@ data "aws_iam_policy_document" "assume_role" {
  
     actions = ["sts:AssumeRole"]
   }
-}
- 
-resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
 } 
  
 resource "aws_lambda_function" "test_lambda" {
@@ -21,7 +16,7 @@ resource "aws_lambda_function" "test_lambda" {
   # path.module in the filename.
   filename      = "function.zip"
   function_name = "greeting-timestamp-lambda_3_tf"
-  role          = aws_iam_role.iam_for_lambda.arn
+  #role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.lambda_handler"
  
   source_code_hash = filebase64sha256("function.zip")
