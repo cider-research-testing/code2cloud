7b5a340a0aa9b5943d7cf012b730f8c6024d54a2
yaron-cider <100203813+yaron-cider@users.noreply.github.com>
2025-02-13 12:50:27 +0200

    Update build-ami.tf

--- build-ami/build-ami.tf
+++ build-ami/build-ami.tf
@@ -17,15 +17,9 @@ variable "aws_region" {
   description = "AWS region to deploy to"
 }
 
-variable "private_key_path" {
-  type = string
-  description = "Path to your private key file"
-  default = "~/.ssh/id_rsa" #Added a default value
-}
-
 resource "aws_instance" "ami_builder" {
-  ami           = "ami-0c55b9fdcb30b5a97"  # Replace with a suitable Ubuntu AMI ID for your region
-  instance_type = "t2.micro"  # Or a more appropriate instance type
+  ami           = "ami-04b4f1a9cf54c11d0"  # Replace with a suitable Ubuntu AMI ID for your region
+  instance_type = "t3.micro"  # Or a more appropriate instance type
 
   tags = {
     Name = "AMI Builder"
@@ -47,8 +41,6 @@ resource "aws_instance" "ami_builder" {
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get update",
-      "sudo apt-get install -y python3 python3-pip",  # Install Python (if not already present)
-      "sudo pip3 install boto3", # install boto3
       "sudo python3 /tmp/main.py",  # Execute the script
     ]
   }
@@ -56,7 +48,6 @@ resource "aws_instance" "ami_builder" {
   connection {
     type        = "ssh"
     user        = "ubuntu"  # Or the appropriate user for your AMI
-    private_key = file(var.private_key_path) # Use the private key
     host        = self.public_ip
   }
 }
@@ -64,20 +55,7 @@ resource "aws_instance" "ami_builder" {
 resource "aws_ami" "generated_ami" {
   name        = "my-custom-ami"
   description = "AMI built with Terraform and GitHub Actions"
-  virtualization_type = "hvm"
-  root_device_name = "/dev/sda1"
-
-  source_instance_id = aws_instance.ami_builder.id
 }
-
-resource "null_resource" "cleanup" {
-  depends_on = [aws_ami.generated_ami]
-
-  provisioner "local-exec" {
-    command = "aws ec2 terminate-instances --instance-ids ${aws_instance.ami_builder.id} --region ${var.aws_region}"
-  }
-}
-
 
 output "ami_id" {
   value = aws_ami.generated_ami.id
