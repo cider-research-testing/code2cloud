0e300fbae3a06828d435a9842951a3523504409e
yaron-cider <100203813+yaron-cider@users.noreply.github.com>
2025-02-16 11:25:45 +0200

    Update build-ami.tf

--- build-ami/build-ami.tf
+++ build-ami/build-ami.tf
@@ -51,7 +51,7 @@ resource "aws_security_group" "aws_ami_test3" {
 resource "aws_instance" "ami_builder" {
   ami           = "ami-04b4f1a9cf54c11d0"  # Replace with a suitable Ubuntu AMI ID for your region
   instance_type = "t3.micro"  # Or a more appropriate instance type
-  vpc_security_group_ids = ["${aws_security_group.test3.id}"]
+  vpc_security_group_ids = ["${aws_security_group.aws_ami_test3.id}"]
   key_name      = "my-key-pair"
   tags = {
     Name = "AMI Builder"
